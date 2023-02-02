9ca6ef2428fb8af1c17e1a7b76351d2671b42d84
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2023-02-02 11:37:14 +0700

    listview

--- lib/todo_screen.dart
+++ lib/todo_screen.dart
@@ -11,19 +11,33 @@ class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          for (int i = 0; i < 20; i++)
-            const ListTile(
-              leading: null,
-              title: Text('Judul Todo'),
-              subtitle: Text(
-                'Ini adalah deskripsi tulisan tulisan',
-                overflow: TextOverflow.ellipsis,
+      body: Dismissible(
+        key: UniqueKey(),
+        direction: DismissDirection.horizontal,
+        onDismissed: (_) {},
+        child: ListView(
+          children: [
+            for (int i = 0; i < 20; i++)
+              ListTile(
+                leading: null,
+                title: const Text('Judul Todo'),
+                subtitle: const Text(
+                  'Ini adalah deskripsi tulisan tulisan',
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () {},
               ),
-              trailing: Icon(Icons.chevron_right),
-            )
-        ],
+            const SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
